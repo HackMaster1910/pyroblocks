8844b2db6e39659777a671940e08bd0eaef97c20
HackMaster1910 <80475386+HackMaster1910@users.noreply.github.com>
2021-12-28 10:40:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 from distutils.core import setup
 setup(
-  name = 'py-roblocks',         # How you named your package folder (MyLib)
-  packages = ['py-roblocks'],   # Chose the same as "name"
+  name = 'pyroblocks',         # How you named your package folder (MyLib)
+  packages = ['pyroblocks'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A roblox API wrapper.',   # Give a short description about your library
